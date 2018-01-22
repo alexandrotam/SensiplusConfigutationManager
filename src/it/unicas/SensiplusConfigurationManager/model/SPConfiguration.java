c3e14050b86625414adb7ebed1b5d6359e083a0c
alexandrotam <0035351@studentmail.unicas.it>
2018-01-22 11:43:56 +0100

    SPConfiguration

---
+++ src/it/unicas/SensiplusConfigurationManager/model/SPConfiguration.java
@@ -0,0 +1,179 @@
+package it.unicas.SensiplusConfigurationManager.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SPConfiguration {
+
+
+    private StringProperty idSPConfiguration;
+    private StringProperty driver;
+    private StringProperty hostController;
+    private StringProperty apiOwner;
+    private StringProperty mcu;
+    private StringProperty protocol;
+    private StringProperty addressingType;
+    private StringProperty idCluster;
+
+
+
+    public SPConfiguration(String idSPConfiguration,String driver, String hostController,String apiOwner,String mcu,
+                           String protocol,String addressingType,String idCluster){
+        this.idSPConfiguration = new SimpleStringProperty(idSPConfiguration);
+        this.driver=new SimpleStringProperty(driver);
+        this.hostController=new SimpleStringProperty(hostController);
+        this.apiOwner=new SimpleStringProperty(apiOwner);
+        this.mcu=new SimpleStringProperty(mcu);
+        this.protocol=new SimpleStringProperty(protocol);
+        this.addressingType=new SimpleStringProperty(addressingType);
+        this.idCluster=new SimpleStringProperty(idCluster);
+
+        if ( idSPConfiguration != null){
+            this.idSPConfiguration = new SimpleStringProperty(idSPConfiguration);
+        } else {
+            this.idSPConfiguration = null;
+        }
+
+    }
+
+
+    public SPConfiguration(){
+        this("","","","","","","","");
+    }
+
+    public String getidSPConfiguration() {
+        if (idSPConfiguration == null){
+            idSPConfiguration = new SimpleStringProperty(null);
+        }
+        return idSPConfiguration.get();
+    }
+
+
+    public void setidSPConfiguration(String idSPConfiguration) {
+        if (this.idSPConfiguration == null){
+            this.idSPConfiguration = new SimpleStringProperty();
+        }
+        this.idSPConfiguration.set(idSPConfiguration);
+    }
+
+    public StringProperty idSPConfigurationProperty() {
+        return idSPConfiguration;
+    }
+
+    public String getDriver() {
+        return driver.get();
+    }
+
+    public StringProperty driverProperty() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver.set(driver);
+    }
+
+    public String getHostController() {
+        return hostController.get();
+    }
+
+    public StringProperty hostControllerProperty() {
+        return hostController;
+    }
+
+    public void setHostController(String hostController) {
+        this.hostController.set(hostController);
+    }
+
+    public String getApiOwner() {
+        return apiOwner.get();
+    }
+
+    public StringProperty apiOwnerProperty() {
+        return apiOwner;
+    }
+
+    public void setApiOwner(String apiOwner) {
+        this.apiOwner.set(apiOwner);
+    }
+
+    public String getMcu() {
+        return mcu.get();
+    }
+
+    public StringProperty mcuProperty() {
+        return mcu;
+    }
+
+    public void setMcu(String mcu) {
+        this.mcu.set(mcu);
+    }
+
+    public String getProtocol() {
+        return protocol.get();
+    }
+
+    public StringProperty protocolProperty() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol.set(protocol);
+    }
+
+    public String getAddressingType() {
+        return addressingType.get();
+    }
+
+    public StringProperty addressingTypeProperty() {
+        return addressingType;
+    }
+
+    public void setAddressingType(String addressingType) {
+        this.addressingType.set(addressingType);
+    }
+
+    public String getIdCluster() {
+        return idCluster.get();
+    }
+
+    public StringProperty idClusterProperty() {
+        return idCluster;
+    }
+
+    public void setIdCluster(String idCluster) {
+        this.idCluster.set(idCluster);
+    }
+
+    public String toString(){
+        return idSPConfiguration.getValue()+", " +driver.getValue()+", "+hostController.getValue()+", "+apiOwner.getValue()
+                +", "+mcu.getValue()+", "+protocol.getValue()+", "+addressingType.get()+", "+idCluster.getValue();
+    }
+
+
+    public static void main(String[] args) {
+        SPConfiguration spConfiguration = new SPConfiguration();
+
+
+        // Use Java Collections to create the List.
+        List<SPConfiguration> list = new ArrayList<>();
+
+        // Now add observability by wrapping it with ObservableList.
+        ObservableList<SPConfiguration> observableList = FXCollections.observableList(list);
+        observableList.addListener(new ListChangeListener() {
+
+            @Override
+            public void onChanged(ListChangeListener.Change change) {
+                System.out.println("Detected a change! ");
+            }
+        });
+
+        System.out.println("Size: "+observableList.size());
+    }
+
+}
---
+++ src/it/unicas/SensiplusConfigurationManager/model/dao/mysql/SPConfigurationDAOMySQLImpl.java
@@ -0,0 +1,208 @@
+package it.unicas.SensiplusConfigurationManager.model.dao.mysql;
+
+import it.unicas.SensiplusConfigurationManager.model.SPConfiguration;
+import it.unicas.SensiplusConfigurationManager.model.dao.DAOException;
+import it.unicas.SensiplusConfigurationManager.model.dao.DAOSPConfiguration;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class SPConfigurationDAOMySQLImpl implements DAOSPConfiguration<SPConfiguration>{
+
+    private SPConfigurationDAOMySQLImpl(){}
+
+    private static DAOSPConfiguration dao = null;
+    private static Logger logger = null;
+    public static DAOSPConfiguration getInstance(){
+        if (dao == null){
+            dao= (DAOSPConfiguration) new SPConfigurationDAOMySQLImpl();
+            logger = Logger.getLogger(SPConfigurationDAOMySQLImpl.class.getName());
+
+        }
+        return dao;
+    }
+
+    public static void main(String args[]) throws DAOException {
+        SPConfigurationDAOMySQLImpl s = new SPConfigurationDAOMySQLImpl();
+
+        List<SPConfiguration> list=s.select((SPConfiguration) null);
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+
+        SPConfiguration toDelete = new SPConfiguration();
+        toDelete.setidSPConfiguration("");
+        toDelete.setDriver("");
+        toDelete.setHostController("");
+        toDelete.setApiOwner("");
+        toDelete.setMcu("");
+        toDelete.setProtocol("");
+        toDelete.setAddressingType("");
+        toDelete.setIdCluster("");
+        s.delete(toDelete);
+
+        list = s.select((SPConfiguration) null);
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+    }
+
+
+    @Override
+    public List<SPConfiguration> select(SPConfiguration a) throws DAOException {
+
+        if (a == null){
+            a=new SPConfiguration(null,null,null,null,null,null,null,null);
+        }
+
+        ArrayList<SPConfiguration> lista = new ArrayList<SPConfiguration>();
+        try{
+
+            if (a == null || a.getidSPConfiguration() == null
+                    || a.getDriver() == null
+                    || a.getHostController() == null
+                    || a.getApiOwner() == null
+                    || a.getMcu() == null
+                    || a.getProtocol() == null
+                    || a.getAddressingType()==null
+                    || a.getIdCluster()==null)
+            {
+                throw new DAOException("In select: any field can be null");
+            }
+
+            Statement st = DAOMySQLSettings.getStatement();
+
+            String sql = "select * from SPConfiguration where idSPConfiguration like '";
+            sql += a.getidSPConfiguration() + "%' and driver  like '"+a.getDriver();
+            sql += "%' and  hostController like '" + a.getHostController() + "%'";
+            sql += " and  apiOwner like '" + a.getApiOwner() + "%'";
+            sql += " and  mcu like '" + a.getMcu() + "%'";
+            sql += " and  protocol like '" + a.getProtocol() + "%'";
+            sql += " and  addressingType like '" + a.getAddressingType() + "%'";
+            sql += " and  idCluster like '" + a.getIdCluster() + "%'";
+
+
+
+            logger.info("SQL: " + sql);
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+                lista.add(new SPConfiguration(rs.getString("idSPConfiguration"),rs.getString("driver"),
+                        rs.getString("hostController"),rs.getString("apiOwner"),rs.getString("mcu"),
+                        rs.getString("protocol"),rs.getString("addressingType"),rs.getString("idCluster")));
+            }
+            DAOMySQLSettings.closeStatement(st);
+
+        } catch (SQLException sq){
+            throw new DAOException("In select(): " + sq.getMessage());
+        }
+        return lista;
+    }
+
+
+    @Override
+    public void delete(SPConfiguration a) throws DAOException {
+        if (a == null || a.getIdCluster() == null
+                || a.getDriver() == null
+                || a.getHostController() == null
+                || a.getApiOwner() == null
+                || a.getMcu() == null
+                || a.getProtocol() == null
+                || a.getAddressingType()==null
+                || a.getIdCluster()==null)
+        {
+            throw new DAOException("In delete: any field can be null");
+        }
+        String query = "DELETE  FROM SPConfiguration WHERE idSPConfiguration='" + a.getidSPConfiguration() + "';";
+        logger.info("SQL: " + query);
+
+        Statement st = null;
+        try {
+            st = DAOMySQLSettings.getStatement();
+            int n = st.executeUpdate(query);
+
+            DAOMySQLSettings.closeStatement(st);
+
+        } catch (SQLException e) {
+            throw new DAOException("In delete(): " + e.getMessage());
+        }
+    }
+
+
+    @Override
+    public void insert(SPConfiguration a) throws DAOException {
+
+
+        if (a == null || a.getidSPConfiguration() == ""
+                || a.getDriver() == null
+                || a.getHostController() == null
+                || a.getApiOwner() == null
+                || a.getMcu() == null
+                || a.getProtocol() == null
+                || a.getAddressingType()==null
+                || a.getIdCluster()==null)
+        {
+            throw new DAOException("In select: any field can be null");
+        }
+
+
+        String query ="INSERT INTO SPConfiguration (driver,hostController,apiOwner,mcu,protocol,addressingType,idCluster)" +
+                " VALUES " +" ('" +a.getDriver()
+                + "', '" +a.getHostController()+ "', '" +a.getApiOwner()
+                + "', '" +a.getMcu()+ "', '" +a.getProtocol()
+                + "', '" +a.getAddressingType()+ "', '" +a.getIdCluster()+"')";
+
+        logger.info("SQL: " + query);
+
+        try {
+            Statement st = DAOMySQLSettings.getStatement();
+            int n = st.executeUpdate(query);
+
+            DAOMySQLSettings.closeStatement(st);
+
+        } catch (SQLException e) {
+            throw new DAOException("In update(): " + e.getMessage());
+        }
+    }
+
+
+    @Override
+    public void update(SPConfiguration a) throws DAOException {
+        if (a == null || a.getidSPConfiguration() == ""
+                || a.getDriver() == null
+                || a.getHostController() == null
+                || a.getApiOwner() == null
+                || a.getMcu() == null
+                || a.getProtocol() == null
+                || a.getAddressingType()==null
+                || a.getIdCluster()==null)
+        {
+            throw new DAOException("In select: any field can be null");
+        }
+
+        String query = "UPDATE SPConfiguration s SET s.idSPConfiguration = '" + a.getidSPConfiguration() + "', s.driver = '" + a.getDriver()
+                + "',  s.hostController = '" + a.getHostController() + "'," +" s.apiOwner = '"+ a.getApiOwner() +
+                "', s.mcu = '" + a.getMcu()+"', s.protocol='"+a.getProtocol()+"',s.addressingType='"+a.getAddressingType()
+                +"', s.idCluster='"+a.getIdCluster()+"';";
+        query = query + " WHERE s.idSPConfiguration = '" + a.getidSPConfiguration() + "';";
+        logger.info("SQL: " + query);
+
+        try {
+            Statement st = DAOMySQLSettings.getStatement();
+            int n = st.executeUpdate(query);
+
+            DAOMySQLSettings.closeStatement(st);
+
+        } catch (SQLException e) {
+            throw new DAOException("In update(): " + e.getMessage());
+        }
+    }
+
+
+}
